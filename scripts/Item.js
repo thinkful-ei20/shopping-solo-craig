bfb4bdb7a2c4c9765fbda285f28adf8341de3b85
Craig Pounds <nweaver@windstream.net>
2018-04-10 20:42:38 -0500

    wrote Item module

--- scripts/Item.js
+++ scripts/Item.js
@@ -1,4 +1,21 @@
+/* global cuid */
 'use strict';
 
+const Item = (function() {
+  const validateName = function(name) {
+    if (!name) throw new TypeError('');
+  };
 
+  const create = function(name) {
+    return {
+      id: cuid(),
+      name: name,
+      checked: false,
+    };
+  };
 
+  return {
+    validateName,
+    create,
+  };
+}());
